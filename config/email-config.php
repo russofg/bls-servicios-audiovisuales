ff7a41f223622f6692a7b0dd94dbcae438d18d0b
russofg <russofg@gmail.com>
2025-05-26 17:48:20 -0300

    logos de clientes

--- config/email-config.php
+++ config/email-config.php
@@ -1,7 +1,6 @@
 <?php
 // Configuración SMTP segura para PHPMailer
-define('SMTP_HOST', 'smtp.hostinger.com');       
-define('SMTP_USER', 'contacto@serviciosbls.com
-');    
-define('SMTP_PASS', 'Pincharrata160$');      
-define('SMTP_PORT', 465);                         
+define('SMTP_HOST', 'smtp.hostinger.com');
+define('SMTP_USER', 'contacto@serviciosbls.com');
+define('SMTP_PASS', 'Pincharrata160$');
+define('SMTP_PORT', 465);
